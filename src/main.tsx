3b9c5240d37a4d9fa17d7360c1c7bf5c30ae19bf
mefremov <mefremov@bescargo.ru>
2025-12-19 14:46:08 +0300

    add: add lib tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "./app/providers/router";
 import "./index.css";
-
 createRoot(document.getElementById("root")!).render(<RouterProvider />);
--- src/pages/login/ui/LoginPage.tsx
+++ src/pages/login/ui/LoginPage.tsx
@@ -1,5 +1,9 @@
 export default function LoginPage() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-3xl">Page login</h1>
+    </>
+  );
 }
 
 // здесь я получаю ввод userId + делаю проверку /users/:id. Если ок, то передаю userId в LoginPageView
