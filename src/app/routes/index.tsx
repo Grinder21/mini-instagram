7437b9c00aa9262421348f3f942da8b726297a89
Grinder21 <spartawinner280@mail.ru>
2025-12-21 17:14:11 +0300

    edit: edit router and add 404 page

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,9 +1,10 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import LoginPage from "../../pages/login/ui/LoginPage";
-import UserPage from "../../pages/user/ui/UserPage";
-import AlbumPage from "../../pages/albums/ui/AlbumPage";
-import PhotoPage from "../../pages/photos/ui/PhotoPage";
-import SettingsPage from "../../pages/settings/ui/SettingsPage";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "@pages/login/ui/LoginPage";
+import UserPage from "@pages/user/ui/UserPage";
+import AlbumPage from "@pages/albums/ui/AlbumPage";
+import PhotoPage from "@pages/photos/ui/PhotoPage";
+import SettingsPage from "@pages/settings/ui/SettingsPage";
+import NotFoundPage from "@/pages/notfound/ui/NotFoundPage";
 
 export function AppRoutes() {
   return (
@@ -13,7 +14,7 @@ export function AppRoutes() {
       <Route path="/albums/:id" element={<AlbumPage />} />
       <Route path="/photos/:id" element={<PhotoPage />} />
       <Route path="/settings" element={<SettingsPage />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
---
+++ src/pages/notfound/ui/NotFoundPage.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <>
+      <div>Ошибка 404. Страница не найдена.</div>
+    </>
+  );
+}
